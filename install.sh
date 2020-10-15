894fd7ed4039ac6cebfcd221413a601ac7108bbc
Devon R <sansgorialis@gmail.com>
2020-10-15 18:49:17 +0900

    OpenSUSE Leap & Tumbleweed support in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Ubuntu
+# Ubuntu (apt)
 if command -v apt &> /dev/null; then
-    echo "Installing packages for Ubuntu"
+    echo "Installing packages for Ubuntu (apt)"
 
     sudo apt install -y git python3 python3-pip build-essential binutils-mips-linux-gnu zlib1g-dev libyaml-dev gcc-multilib || exit 1
     python3 -m pip install capstone
@@ -17,9 +17,9 @@ if command -v apt &> /dev/null; then
     exit
 fi
 
-# Arch
+# Arch Linux (pacman)
 if command -v pacman &> /dev/null; then
-    echo "Installing packages for Arch"
+    echo "Installing packages for Arch Linux (pacman)"
 
     # Upgrade existing packages (note: no --noconfirm)
     sudo pacman -Syu || exit 1
@@ -58,6 +58,45 @@ if command -v pacman &> /dev/null; then
     exit
 fi
 
-echo "Only Ubuntu (apt) and Arch Linux (pacman) are supported by install.sh."
+# openSUSE (zypper)
+if command -v zypper &> /dev/null; then
+    echo "Installing packages for openSUSE (zypper)"
+
+    sudo zypper -n install git python3 python3-devel python3-pip gcc gcc-c++ glibc-devel make cross-mips-binutils zlib-devel libyaml-devel gcc-32bit gcc-c++-32bit glibc-devel-32bit
+
+    # Link the openSUSE locations for binutils tools to their usual GNU locations
+    sudo ln -s /usr/bin/mips-suse-linux-addr2line /usr/bin/mips-linux-gnu-addr2line
+    sudo ln -s /usr/bin/mips-suse-linux-ar /usr/bin/mips-linux-gnu-ar
+    sudo ln -s /usr/bin/mips-suse-linux-as /usr/bin/mips-linux-gnu-as
+    sudo ln -s /usr/bin/mips-suse-linux-elfedit /usr/bin/mips-linux-gnu-elfedit
+    sudo ln -s /usr/bin/mips-suse-linux-gprof /usr/bin/mips-linux-gnu-gprof
+    sudo ln -s /usr/bin/mips-suse-linux-ld /usr/bin/mips-linux-gnu-ld
+    sudo ln -s /usr/bin/mips-suse-linux-ld.bfd /usr/bin/mips-linux-gnu-ld.bfd
+    sudo ln -s /usr/bin/mips-suse-linux-nm /usr/bin/mips-linux-gnu-nm
+    sudo ln -s /usr/bin/mips-suse-linux-objcopy /usr/bin/mips-linux-gnu-objcopy
+    sudo ln -s /usr/bin/mips-suse-linux-objdump /usr/bin/mips-linux-gnu-objdump
+    sudo ln -s /usr/bin/mips-suse-linux-ranlib /usr/bin/mips-linux-gnu-ranlib
+    sudo ln -s /usr/bin/mips-suse-linux-readelf /usr/bin/mips-linux-gnu-readelf
+    sudo ln -s /usr/bin/mips-suse-linux-size /usr/bin/mips-linux-gnu-size
+    sudo ln -s /usr/bin/mips-suse-linux-strings /usr/bin/mips-linux-gnu-strings
+    sudo ln -s /usr/bin/mips-suse-linux-strip /usr/bin/mips-linux-gnu-strip
+
+    python3 -m pip install capstone
+
+    if [[ $1 == "--extra" ]]; then
+        echo "Installing extra"
+        sudo zypper -n install clang astyle || exit 1
+        python3 -m pip install stringcase || exit 1
+    fi
+
+    echo "Done"
+    exit
+fi
+
+
+echo "Only the following distros are supported by install.sh:"
+echo "- Ubuntu (apt)"
+echo "- Arch Linux (pacman)"
+echo "- openSUSE (zypper)"
 echo "Please consider contributing and adding an installation script for your distro."
 exit 1
